705bc7ec13cc7d40b12b8a8f89b609edec8c473d
JK Gunnink <jgunnink@gmail.com>
2017-12-11 22:09:48 +0800

    Add charts and maps to weather data

--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -1,12 +1,31 @@
 import React from "react"
+import _ from "lodash"
 import { connect } from "react-redux"
+import Chart from "../components/chart"
+import GoogleMap from "../components/google_map"
 
 class WeatherList extends React.Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), temp => temp - 273)
+		const pressures = cityData.list.map(weather => weather.main.pressure)
+		const humidities = cityData.list.map(weather => weather.main.humidity)
+		const { lon, lat } = cityData.city.coord
+
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td>
+					<GoogleMap lon={lon} lat={lat} />
+				</td>
+				<td>
+					<Chart data={temps} colour="orange" units="C" />
+				</td>
+				<td>
+					<Chart data={pressures} colour="green" units="hPa" />
+				</td>
+				<td>
+					<Chart data={humidities} colour="black" units="%" />
+				</td>
 			</tr>
 		)
 	}
@@ -16,9 +35,9 @@ class WeatherList extends React.Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>{this.props.weather.map(this.renderWeather)}</tbody>
